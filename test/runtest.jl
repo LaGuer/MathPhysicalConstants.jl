d52469f131366ad950ed69cfda60450c2f8acd35
Laurent G. OSSBSS AMIS UCC <lgueroult@hotmail.com>
2018-12-11 12:39:19 +0100

    Update runtest.jl

--- test/runtest.jl
+++ test/runtest.jl
@@ -1,5 +1,16 @@
-using MathPhysicalConstants
-using Base.Test
+using MathPhysicalConstants, Measurements, Unitful
 
-# write your own tests here
-@test 1 == 1
+using Test
+
+using MathPhysicalConstants.SI
+
+@testset begin
+    @test ustrip(big(h)) == big"6.626070040e-34"
+    @test setprecision(BigFloat, 768) do; precision(ustrip(big(c))) end == 768
+    @test measurement(h) === measurement(h)
+    @test iszero(measurement(α) - measurement(α))
+    @test isone(measurement(BigFloat, atm) / measurement(BigFloat, atm))
+    @test iszero(measurement(BigFloat, ħ) - (measurement(BigFloat, h) / 2big(pi)))
+    @test isone(measurement(BigFloat, ħ) / (measurement(BigFloat, h) / 2big(pi)))
+end
+
