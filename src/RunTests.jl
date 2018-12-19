1427ded61d94a978a585a17f2ef0219ac6b7b454
Laurent G. OSSBSS AMIS UCC <lgueroult@hotmail.com>
2018-12-19 19:29:53 +0100

    Update RunTests.jl

--- src/RunTests.jl
+++ src/RunTests.jl
@@ -1,11 +1,28 @@
-using PyCall
+using MathPhysicalConstants
+using Base.Test
+using Test
+include("../src/RunTests.jl")
+
+@testset begin
+    @test MathPhysicalConstants.MKS.PlancksConstantH == 6.62606896e-34
+    @test MathPhysicalConstants.SI.PlanckConstantH == 6.62607015e-34
+    @test MathPhysicalConstants.SI.SanchezElectrConstant == 137.035999139
+    #\\\ @test iszero(measurement(MathPhysicalConstants.IS.SanchezElectrConstant) - measurement(MathPhysicalConstants.IS.SanchezElectrConstant))
+    # \\\ @test setprecision(BigFloat, 768) do; precision(ustrip(big(c))) end == 768
+    #\\\@test measurement(h) === measurement(h)
+    #\\\@test iszero(measurement(α) - measurement(α))
+    #\\\@test isone(measurement(BigFloat, atm) / measurement(BigFloat, atm))
+    #\\\@test iszero(measurement(BigFloat, ħ) - (measurement(BigFloat, h) / 2big(pi)))
+    #\\\@test isone(measurement(BigFloat, ħ) / (measurement(BigFloat, h) / 2big(pi)))
+end
+
+#using PyCall
 #const Chem = PyNULL()
 #const molecule = PyNULL()
 #const pydot = PyNULL()
 #copy!(Chem,pyimport_conda("rdkit.Chem","rdkit","rmg"))
 #copy!(molecule,pyimport_conda("rmgpy.molecule","rmgpy","rmg"))
 #copy!(pydot,pyimport_conda("pydot","pydot","rmg"))
-
 
 #include("Constants.jl")
 #include("Tools.jl")
