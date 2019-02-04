e6b212dc760dae66c7d2d94415d71e60daacad34
Laurent G. OSSBSS AMIS UCC <lgueroult@hotmail.com>
2019-02-04 04:47:49 -0800

    Update mpctest.jl

--- src/mpctest.jl
+++ src/mpctest.jl
@@ -5,9 +5,10 @@ using Test
 @testset begin
     @test MathPhysicalConstants.MKS.PlancksConstantH == 6.62606896e-34
     @test MathPhysicalConstants.SI.PlanckConstantH == 6.62607015e-34
-    @test MathPhysicalConstants.SI.SanchezElectrConstant == 137.035999139
+    @test MathPhysicalConstants.SI.SanchezElectrConstant == 137.035999139e0
+    @test MathPhysicalConstants.CGS.GravitationalConstant == 6.673e-8
     #\\\ @test iszero(measurement(MathPhysicalConstants.IS.SanchezElectrConstant) - measurement(MathPhysicalConstants.IS.SanchezElectrConstant))
-    # \\\ @test setprecision(BigFloat, 768) do; precision(ustrip(big(c))) end == 768
+    #\\\ @test setprecision(BigFloat, 768) do; precision(ustrip(big(c))) end == 768
     #\\\@test measurement(h) === measurement(h)
     #\\\@test iszero(measurement(α) - measurement(α))
     #\\\@test isone(measurement(BigFloat, atm) / measurement(BigFloat, atm))
