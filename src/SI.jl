9dc48a5dfe932645780f37cc19e9338d6cfc2a05
Laurent G. OSSBSS AMIS UCC <lgueroult@hotmail.com>
2019-02-04 00:41:05 -0800

    Update SI.jl

--- src/SI.jl
+++ src/SI.jl
@@ -112,9 +112,9 @@ module SI
     CmbTemperature = 2.725820831;                           # K
     UniverseSanchezMass = 8.7936e52;                        # kg
     SanchezElectrConstant = 137.035999139e0;                # dimensionless Sanchez Electric Constant
-    α = 7.2973525664e10-3;                                  # Fine Structure Constant
-    ε0 =  8.854187817e10-12;                                # F m-1 electric constant
-    μ0 =  12.566370614e10-7;                                # N A-2     magnetic constant 
+    α = 7.2973525664e-3;                                    # Fine Structure Constant
+    ε0 =  8.854187817e-12;                                  # F m-1 electric constant
+    μ0 =  12.566370614e-7;                                  # N A-2     magnetic constant 
     γ = 0.5772156649015328606065120900824024310421593359399e0 # Euler–Mascheroni constant
     e = 2.7182818284590450907955982984276488423347473144531e0 # Euler
     a = 137.03599913815450976591038346353016906084731063214e0 # Sanchez Electric Constant
